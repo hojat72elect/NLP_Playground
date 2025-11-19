e6b13b65b72bb0077790b522d96c2bc7d008f6a3
hojat72elect <hojat72elect@gmail.com>
2025-11-19 00:30:27 -0500

    Completed the example 1.

--- JavaScript/src/example1_tokens.js
+++ JavaScript/src/example1_tokens.js
@@ -1,7 +1,7 @@
 import winkNLP from 'wink-nlp';
 import model from 'wink-eng-lite-web-model';
 
-const nlp = winkNLP( model );
+const nlp = winkNLP(model);
 
 const text = "Hello   World🌎! How are you?\nNames are for friends, so I do not need one. 😏";
 const doc = nlp.readDoc(text); // Reading a text
@@ -9,3 +9,9 @@ const doc = nlp.readDoc(text); // Reading a text
 console.log(doc.out(), "\n"); // Logging the contents of the document
 
 console.log(doc.tokens().out(nlp.its.type, nlp.as.freqTable)); // token types and their frequencies
+
+console.log(doc.sentences().out()); // Breaks the whole text into separate sentences.
+
+console.log(doc.entities().out(nlp.its.detail)); // ?
+
+console.log(doc.tokens().out()); // Break the text into tokens.
