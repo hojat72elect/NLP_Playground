520acfb02f1f3b8fb77ed68752e65191ea2bdc99
hojat72elect <hojat72elect@gmail.com>
2025-11-18 20:32:42 -0500

    wink js example - token types and their frequencies.

--- JavaScript/src/Main.js
+++ JavaScript/src/Main.js
@@ -1,1 +1,11 @@
-console.log("Erin and CC");
+import winkNLP from 'wink-nlp';
+import model from 'wink-eng-lite-web-model';
+
+const nlp = winkNLP( model );
+
+const text = "Hello   World🌎! How are you?\nNames are for friends, so I do not need one. 😏";
+const doc = nlp.readDoc(text); // Reading a text
+
+console.log(doc.out(), "\n"); // Logging the contents of the document
+
+console.log(doc.tokens().out(nlp.its.type, nlp.as.freqTable)); // token types and their frequencies
